bdcd9bca7aebee2e0442fdcfe174f6f196869fb6
mikitomi21 <kubastach2001@gmail.com>
2023-08-01 17:53:52 +0200

    Create board of squares

--- chess_tests/test_gui.py
+++ chess_tests/test_gui.py
@@ -7,17 +7,17 @@ class Test_gui(unittest.TestCase):
         self.root.geometry("1280x800")
         self.board = Board(self.root)
 
-    def test_color_of_square_chang_after_click(self):
-        initial_color = self.board.canvas.itemcget(self.board.rectangle, "fill")
+    def test_changing_color_of_square_after_click(self):
+        initial_color = self.board.squares[0][0].canvas.itemcget(self.board.squares[0][0].rectangle, "fill")
         self.assertEqual(initial_color, "blue")
 
-        self.board.click_square(None)
+        self.board.squares[0][0].click_square(None)
 
-        initial_color = self.board.canvas.itemcget(self.board.rectangle, "fill")
+        initial_color = self.board.squares[0][0].canvas.itemcget(self.board.squares[0][0].rectangle, "fill")
         self.assertEqual(initial_color, "red")
 
-        self.board.click_square(None)
+        self.board.squares[0][0].click_square(None)
 
-        initial_color = self.board.canvas.itemcget(self.board.rectangle, "fill")
+        initial_color = self.board.squares[0][0].canvas.itemcget(self.board.squares[0][0].rectangle, "fill")
         self.assertEqual(initial_color, "blue")
 
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import tkinter as tk
 from constants import *
-class Board:
-    def __init__(self, root: tk.Tk):
-        self.canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
-        self.canvas.pack()
 
+class Square:
+    def __init__(self, canvas: tk.Canvas, row: int, col: int):
+        self.canvas = canvas
         self.is_clicked = False
+        self.row = row
+        self.col = col
 
-        x1, y1 = 50, 50
-        x2, y2 = 50 + LENGHT_OF_SQUARE, 50 + LENGHT_OF_SQUARE
+        x1, y1 = START_X + LENGHT_OF_SQUARE * row, START_Y + LENGHT_OF_SQUARE * col
+        x2, y2 = START_X + LENGHT_OF_SQUARE * (row+1), START_Y + LENGHT_OF_SQUARE * (col+1)
 
         self.rectangle = self.canvas.create_rectangle(x1, y1, x2, y2, fill="blue")
 
@@ -21,6 +22,25 @@ class Board:
         else:
             self.canvas.itemconfig(self.rectangle, fill="blue")
             self.is_clicked = False
+        print(f"{chr(97+self.row)}{8-self.col}")
+
+class Board:
+    def __init__(self, root: tk.Tk):
+        self.canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
+        self.canvas.pack()
+
+        self.squares = self.create_squares()
+
+    def create_squares(self) -> list[Square]:
+        squares = []
+        for col in range(SIZE_OF_BOARD):
+            row_of_squares = []
+            for row in range(SIZE_OF_BOARD):
+                row_of_squares.append(Square(self.canvas, row, col))
+            squares.append(row_of_squares)
+        return squares
+
+
 
 
 
