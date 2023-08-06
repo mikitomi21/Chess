8e93edd5f19085d211af1f867e09a5613d80316e
mikitomi21 <kubastach2001@gmail.com>
2023-08-06 13:28:13 +0200

    Adjust Point to chess game

--- point.py
+++ point.py
@@ -1,7 +1,8 @@
 class Point:
-    def __init__(self, y: int, x: int):
-        self.y = y
-        self.x = x
+    def __init__(self, position: str):
+        # TODO use some try except assert
+        assert len(position) == 2
+        self.y, self.x = self.get_position(position)
 
-    def get_position(self) -> (int, int):
-        return self.y, self.x
+    def get_position(self, position: str) -> (int, int):
+        return int(position[1]) - 1, ord(position[0]) - 97
