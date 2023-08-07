eece83a168ce5d67ad927009a152a63d96baad7f
mikitomi21 <kubastach2001@gmail.com>
2023-08-07 14:09:20 +0200

    Remove init for Point class

--- point.py
+++ point.py
@@ -1,9 +1,5 @@
 class Point:
-    def __init__(self, position: str):
-        # TODO use some try except assert
-        assert len(position) == 2
-        self.y, self.x = self.get_position(position)
-
     @staticmethod
     def get_position(position: str) -> (int, int):
+        assert len(position) == 2
         return 8 - int(position[1]), ord(position[0]) - 97
