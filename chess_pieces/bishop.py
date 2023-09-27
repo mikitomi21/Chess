bfb1897af5482527b204593a447a604c1f63d466
mikitomi21 <kubastach2001@gmail.com>
2023-09-27 13:34:42 +0200

    Add bishop movement

--- chess_pieces/bishop.py
+++ chess_pieces/bishop.py
@@ -22,11 +22,26 @@ class Bishop(Piece, ABC):
         board.get_square("f8").piece = Bishop("f8", PLAYER_BLACK, board)
         board.get_square("f8").set_image_path("img/black/bishop.png")
 
-    def move(self, position: str) -> None:
-        pass
+    def can_move(self, pos: str) -> bool:
+        y, x = Point.get_position(self.position)
+        y_new, x_new = Point.get_position(pos)
 
-    def can_move(self) -> bool:
-        pass
+        if abs(x - x_new) == abs(y - y_new) and x != x_new:
+            x_dir = 1 if x_new > x else -1
+            y_dir = 1 if y_new > y else -1
+            for i in range(1, abs(x - x_new)):
+                if (
+                    self.board.get_square_ints(y + i * y_dir, x + i * x_dir).piece
+                    is not None
+                ):
+                    return False
+            if (
+                self.board.get_square_ints(y_new, x_new).piece
+                and self.board.get_square_ints(y_new, x_new).piece.player == self.player
+            ):
+                return False
+            return True
+        return False
 
     def __str__(self) -> str:
         return "Bishop"
