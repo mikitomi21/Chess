5cef773b1714d1337f6939af7d741022787e9b53
mikitomi21 <kubastach2001@gmail.com>
2023-09-27 19:03:26 +0200

    Add place for timer

--- clock_manager.py
+++ clock_manager.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 
 from timer import Timer
+<<<<<<< HEAD
 from notation_table import Notation_Table
+=======
+>>>>>>> 66c3da8 (Add place for timer)
 from constants import *
 
 
@@ -11,7 +14,10 @@ class Clock_Manager:
     @classmethod
     def create_timer(cls, root, canvas):
         cls.timer = Timer(root, canvas, 10)
+<<<<<<< HEAD
 
     @classmethod
     def create_notation_table(cls, root, canvas):
         cls.notation_table = Notation_Table(root, canvas)
+=======
+>>>>>>> 66c3da8 (Add place for timer)
